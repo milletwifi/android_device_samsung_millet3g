5e0c9cdc0258de9d575e6c199b6edc928ecbb3cc
DJABHipHop <android@BAServer>
2019-03-29 23:15:04 -0400

    matissewifi -> matisse3g

--- lineage.mk
+++ lineage.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_3gonly.mk)
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/matisse3g/full_matisse3g.mk)
